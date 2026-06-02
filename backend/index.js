3c6a4dcec852cfe455f14b25eb7ab5361022798d
Matheus Da Luz Larini <DaLuzLariniM@cardiff.ac.uk>
2026-06-02 09:32:06 +0100

    adding and testing database connection

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,24 @@
+// loading variables and importing the pool
+require('dotenv').config({path:'../.env'});
+const {Pool} = require('pg');
+
+//Creating a new pool
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{rejectUnauthorized: false }
+})
+// checking connection
+async function testDbConnection(){
+    try{
+        const res = await pool.query('SELECT NOW()');
+        console.log(' Database connected successfully at:', res.rows[0].now);
+    } catch(err){
+        console.error('Connection failed:',err)
+    }
+}
+testDbConnection();
+
+
 const websites = [
     "https://www.google.com/",
     "https://www.mdlmdlmdl.co/",
